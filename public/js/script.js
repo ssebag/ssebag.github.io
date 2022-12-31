926835334200fb7d1b51b40c3c6f9ac76d11399a
seba ghnem <s.sseba96@gmail.com>
2022-12-31 13:36:39 +0200

    update1

---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+var loader=document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display = "none";
+});
+let y=  document.getElementById('alert');
+let x=  document.getElementById('link');
+let w=  document.getElementById('alert1');
+let z=  document.getElementById('link1');
+let w1=  document.getElementById('alert2');
+let z1=  document.getElementById('link2');
+function f(x){
+   y.style.display="block";
+}
+function f2(x){
+  y.style.display="none";
+}
+
+function f1(z){
+    w.style.display="block";
+ }
+ function f3(z){
+   w.style.display="none";
+ }
+ function f11(z1){
+    w1.style.display="block";
+ }
+ function f33(z1){
+   w1.style.display="none";
+ }
