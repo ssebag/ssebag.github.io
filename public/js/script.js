80afc6499b3c730ec9d9f506f627615b80b00621
seba ghnem <s.sseba96@gmail.com>
2023-06-03 17:24:32 +0300

    add website for private school

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,18 @@ var loader=document.getElementById("preloader");
 window.addEventListener("load",function(){
     loader.style.display = "none";
 });
+window.onload=function(){
+  var hi=document.getElementById("hi");
+  var des=document.getElementById("des");
+  var myCV=document.getElementById("buttonCv");
+  var animation=hi.animate([
+    {left:"-150px"},{left:"30px",offset:0.5},{left:"30px",offset:0.5}],{duration:2000,easing:"ease-in"});
+    var animation=des.animate([
+      {left:"1300px"},{left:"30px",offset:0.5},{left:"30px",offset:0.5}],{duration:10000,easing:"ease-out"});
+      var animation=myCV.animate([
+      {top:"-1000px"},{top:"10px",offset:0.5},{top:"10px",offset:1}],{duration:10000,easing:"ease-out"});
+  
+}
 let y=  document.getElementById('alertForRes');
 let x=  document.getElementById('linkForRes');
 /** */
@@ -50,4 +62,20 @@ function f1(z){
  }
  function f33(z1){
    w1.style.display="none";
+ }
+
+ var alert=document.getElementById('alert');
+ function toHover(elem){
+  rect=document.getElementById(elem).getBoundingClientRect();
+  x=rect.x - 20;
+  y=rect.y-240;
+  console.log(x);
+  alert.innerHTML = "<div>"+elem+"</div>"
+  alert.style.position='absolute';
+  alert.style.left=x+'px';
+  alert.style.top=y+'px';
+  alert.style.display="block";
+ }
+ function toDisVisible(){
+  alert.style.display='none';
  }
